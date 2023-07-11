413391d27d88794577fc69fd85fb3a7e05087336
AnandS29 <AnandS29@users.noreply.github.com>
2023-07-11 19:13:23 -0400

    added nice print options to test_render.py

--- tests/test_render.py
+++ tests/test_render.py
@@ -3,12 +3,51 @@ import numpy as np
 import lbforaging
 import time
 
+# arg parser
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--both", action="store_true", help="Run both agents")
+parser.add_argument("--render", action="store_true", help="Render the environment")
+
+args = parser.parse_args()
 # Test rednering
 
 env = gym.make('AltarForaging-7x7-2p-4f-altar-rand-mark-v2')
 obs = env.reset()
 
-env.render()
+def nice_print_ind(obs):
+    altar_x, altar_y, altar_colour = obs[0], obs[1], obs[2]
+    berries = []
+    for i in range(4):
+        berries.append((obs[3+3*i], obs[4+3*i], obs[5+3*i]))
+    pre = 12 + 3
+    players = []
+    for i in range(2):
+        players.append((obs[pre+4*i], obs[pre+4*i+1], obs[pre+4*i+2]), obs[pre+4*i+3])
+
+    print("Altar: ({}, {}) {}".format(altar_x, altar_y, altar_colour))
+    print()
+    for b in berries:
+        print("Berry: ({}, {}) {}".format(b[0], b[1], b[2]))
+    print()
+    i = 0
+    for p in players:
+        if i == 0:
+            print("Player (ME): ({}, {}) lvl={}, marked={}".format(p[0], p[1], p[2], p[3]))
+        else:
+            print("Player: ({}, {}) lvl={}, marked={}".format(p[0], p[1], p[2], p[3]))
+
+def nice_print(obs):
+    print("Agent 1")
+    print(nice_print_ind(obs[0]))
+    print()
+    print("Agent 2")
+    print(nice_print_ind(obs[1]))
+
+if args.render:
+    env.render()
+else:
+    nice_print(obs)
 
 def code_action(action):
     if action == "":
@@ -40,17 +79,21 @@ cum_reward = [0,0]
 for _ in range(1000):
     action1 = input("Action 1: ")
     action1 = code_action(action1)
-    # action2 = input("Action 2: ")
-    # action2 = code_action(action2)
-
-    action2 = np.random.randint(0, 6)
+    if args.both:
+        action2 = input("Action 2: ")
+        action2 = code_action(action2)
+    else:
+        action2 = np.random.randint(0, 6)
     action = (action1, action2)
     obs, reward, done, info = env.step(action)
     cum_reward[0] += reward[0]
     cum_reward[1] += reward[1]
     print(cum_reward)
 
-    env.render()
+    if args.render:
+        env.render()
+    else:
+        nice_print(obs)
 
 # sleep for 10 seconds
 time.sleep(10)
